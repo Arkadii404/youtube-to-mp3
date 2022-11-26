b7c34d2d162eac8900f561996fc63d8fa74c5f1d
Arkadii <arkadii.veretilin@niko-tech.eu>
2022-11-26 23:27:38 +0200

    added audio reply

--- apps/telegram/src/bot/bot.update.ts
+++ apps/telegram/src/bot/bot.update.ts
@@ -1,4 +1,13 @@
-import { Help, InjectBot, On, Message, Start, Update } from 'nestjs-telegraf';
+import {
+  Help,
+  InjectBot,
+  On,
+  Message,
+  Start,
+  Update,
+  Ctx,
+} from 'nestjs-telegraf';
+import * as fs from 'fs';
 import { Telegraf } from 'telegraf';
 import { BotService } from './bot.service';
 import { Context } from '../interfaces/context.interface';
@@ -25,9 +34,15 @@ export class BotUpdate {
   }
 
   @On('text')
-  async onMessage(@Message('text') text: string): Promise<string> {
-    const videoId = await this.youtubeService.downloadAudio(text);
-    console.log('Here', videoId);
-    return videoId;
+  async onMessage(
+    @Message('text') text: string,
+    @Ctx() ctx: Context,
+  ): Promise<string> {
+    const videoPath = await this.youtubeService.downloadAudio(text);
+    await ctx.replyWithAudio({
+      source: fs.readFileSync(videoPath),
+    });
+    console.log('Here', videoPath);
+    return videoPath;
   }
 }
--- libs/youtube/src/youtube.service.ts
+++ libs/youtube/src/youtube.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import * as ytdl from 'ytdl-core';
+import { mkdir } from 'fs/promises';
 import * as fs from 'fs';
+import * as path from 'path';
 import { BadRequestException } from '@nestjs/common';
 
 @Injectable()
@@ -8,20 +10,26 @@ export class YoutubeService {
   public async downloadAudio(url: string): Promise<string> {
     this.validateUrl(url);
 
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       const videoId = ytdl.getVideoID(url);
 
       console.log(`Started download: ${videoId}`);
       console.time(videoId);
 
+      if (!fs.existsSync(path.join(__dirname, 'audios'))) {
+        await mkdir(path.join(__dirname, 'audios'));
+      }
+
+      const pathToVideo = path.join(__dirname, 'audios', `${videoId}.mp3`);
+
       ytdl(url, {
         filter: 'audioonly',
       })
-        .pipe(fs.createWriteStream('video.mp3'))
+        .pipe(fs.createWriteStream(pathToVideo))
         .on('close', () => {
           console.timeEnd(videoId);
           console.log(`Download completed: ${videoId}`);
-          resolve(videoId);
+          resolve(pathToVideo);
         });
     });
   }
